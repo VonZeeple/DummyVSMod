5af5adbd47a14be26b1583508b6d2e4269e9b424
VonZeeple <vonzeeple@gmail.com>
2026-09-19 23:00:33 +0200

    feat: First viable mod.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dummy VS Mod
+
+A mod that does nothing, the only purpose is to test CI/CD.
---
+++ src/DummyVSMod.cs
@@ -0,0 +1,23 @@
+﻿
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo("dummyvsmod",
+    "dummyvsmod",
+    Description = "A simple VS mod for demonstration purposes.",
+    Website = "",
+    Version = ModVersion.Value,
+    Authors = new[] { "PFev" })]
+
+namespace dummyvsmod.src
+{
+    public class DummyVSMod : ModSystem
+    {
+        static string MODID = "dummyvsmod";
+        public override void Start(ICoreAPI api)
+        {
+        }
+
+    }
+}
